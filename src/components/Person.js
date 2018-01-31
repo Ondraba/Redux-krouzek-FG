2a1698a6437dac8e494977c9dedf8d0ee5526cbd
oba <basista@fg.cz>
2018-01-31 08:57:02 +0100

    clear app

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,14 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
-const Person = ({ onClick, person }) => (
-  <li
-    onClick={onClick}
-    style={{
-      color: person.sex === 'women' ? 'red' : 'green'
-    }}
-  >
-    {person.name} | {person.sex}
-  </li>
-)
+// const Person = ({ onClick, person }) => (
+//   <li
+//     onClick={onClick}
+//     style={{
+//       color: person.sex === 'women' ? 'red' : 'green'
+//     }}
+//   >
+//     {person.name} | {person.sex}
+//   </li>
+// )
 
-export default Person
+// export default Person
